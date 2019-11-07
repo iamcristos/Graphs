2b60f5380dd6a1e5f19f8aea7460a8883fc83cb5
icpengineer <icpengineer@intcrzyppl.com>
2019-11-07 17:54:01 +0100

    completed social

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,4 +1,6 @@
-
+import random
+import math
+import time
 
 class User:
     def __init__(self, name):
@@ -30,25 +32,52 @@ class SocialGraph:
         self.users[self.lastID] = User(name)
         self.friendships[self.lastID] = set()
 
-    def populateGraph(self, numUsers, avgFriendships):
-        """
+        def populateGraph(self, numUsers, avgFriendships):
+            """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # Add users
+        # loop over a range of 0 to numUsers
+        for i in range(0, numUsers):
+            # add user to the graph
+            self.addUser(f"User {i}")
+        
+        # create friendships
 
-        # Create friendships
+        # Generate all friendship combinations
+        # make a list of possible friendships
+        possibleFriendships = []
+
+        # avoid duplicates ensuring that the first number is smaller than the second
+        
+        # loop over userID in users
+        for userID in self.users:
+            # loop over friend id in a range from user id + 1 to the lastID +1
+            for friendID in range(userID + 1, self.lastID + 1):
+                # append the tuple of (user id , friend id) to the possible friendships list
+                possibleFriendships.append((userID, friendID))
+        
+        # shuffle the possible friendships using the random.suffle method
+        random.shuffle(possibleFriendships)
+
+        # create afriendships of the first x ammount of pairs in the list   
+        # X determined by the formula: numusers * avgFriendships // 2
+        # we need to divide by to as each createFriendship adds 2 friendships
+        # loop over a range to numUsers * avgFriendships // 2
+        for i in range(0, math.floor(numUsers * avgFriendships / 2)):
+            # set the friendship to possible friends at i
+            friendship = possibleFriendships[i]
+            # addfriendship of friendship[0] and friendship[1]
+            self.addFriendship(friendship[0], friendship[1])
 
     def getAllSocialPaths(self, userID):
         """
@@ -61,6 +90,20 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        q = Queue()
+        q.enqueue([userID])
+        while q.size() > 0:
+            path = q.dequeue()
+            last_vertex = path[-1]
+            if last_vertex not in visited:
+                visited[last_vertex] = path
+                for neighbour in self.friendships[last_vertex]:
+                    # make a copy of the path
+                    path_copy = list(path)
+                    # append neighbour to the coppied path
+                    path_copy.append(neighbour)
+                    # then enqueue the copied path
+                    q.enqueue(path_copy) 
         return visited
 
 
